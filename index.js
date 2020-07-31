d3b686c624c385792522945592cc906081188d8c
unknown <ohndaniel@gmail.com>
2020-07-31 01:05:20 -0700

    Update index.js to link with react client.

--- index.js
+++ index.js
@@ -1,49 +1,60 @@
-require('dotenv/config')
+require("dotenv/config")
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const port = 3001;
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+const port = 3001
 
-const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require("mongodb").MongoClient
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  )
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS")
+  next()
+})
 
 let connect = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.htyqj.mongodb.net/<dbname>?retryWrites=true&w=majority`
 
 MongoClient.connect(connect, { useUnifiedTopology: true })
   .then((client) => {
-    console.log("Connected to Database!");
+    console.log("Connected to Database!")
 
-    const db = client.db("bloggies-db");
-    const blogsCollection = db.collection("blogs");
+    const db = client.db("bloggies-db")
+    const blogsCollection = db.collection("blogs")
 
     // Handlers
-    app.get("/", (req, res) => {
-      const cursor = db
+    app.get(`/blogs`, (req, res) => {
+      const blogs = db
         .collection("blogs")
         .find()
         .toArray()
-        .then((results) => {
-          console.log(results);
+        .then(res)
+        .then((data) => {
+          // console.log(data)
+          res.send(data)
         })
-        .catch((err) => console.log(err));
-
-      res.sendFile(__dirname + "/index.html");
-    });
+        .catch((err) => res.send(err))
+    })
 
     // POST used to create blogs
-    app.post("/create-blog", (req, res) => {
+    app.post(`/create-blog`, (req, res) => {
       blogsCollection
         .insertOne(req.body)
         .then((result) => {
-          res.redirect("/");
+          res.redirect("/")
         })
-        .catch((error) => console.log(error));
-    });
+        .catch((error) => console.log(error))
+    })
 
     app.listen(port, () => {
-      console.log(`listening on localhost:${port}`);
-    });
+      console.log(`listening on http://localhost:${port}`)
+    })
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err))
