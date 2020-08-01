627d05e8d3117b156af8b54a5787a43d107fb188
unknown <ohndaniel@gmail.com>
2020-07-31 23:28:09 -0700

    Readded the res headers.

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const MongoClient = require("mongodb").MongoClient
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  )
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS")
+  next()
+})
+
 let connect = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.htyqj.mongodb.net/<dbname>?retryWrites=true&w=majority`
 
 MongoClient.connect(connect, { useUnifiedTopology: true })
